2659a32c8d2efc086450734c0fecc3662f33849f
VsevolodChaplanov <woornad@gmail.com>
2024-12-03 00:53:41 +0300

    http request examples

--- app/main.cpp
+++ app/main.cpp
@@ -1,72 +1,151 @@
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/spawn.hpp>
+#include <boost/asio/as_tuple.hpp>
+#include <boost/asio/awaitable.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ssl.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
+#include <boost/beast/http/field.hpp>
 #include <boost/beast/version.hpp>
 
-namespace beast = boost::beast; // Boost.Beast
-namespace http = beast::http;   // HTTP from Beast
-namespace net = boost::asio;    // Boost.Asio
-using tcp = net::ip::tcp;       // TCP from Asio
-using namespace std::literals;  // For suffixes like "ms"
+#include <certificates/certificates.hpp>
 
-// Asynchronous function to perform the HTTP GET request
-net::awaitable<void> async_http_get(const std::string& host, const std::string& port) {
-    try {
-        auto executor = co_await net::this_coro::executor;
-        tcp::resolver resolver(executor);
-        tcp::socket socket(executor);
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <ranges>
+#include <string>
 
-        // Resolve the host and connect
-        auto results = co_await resolver.async_resolve(host, port, net::use_awaitable);
-        co_await net::async_connect(socket, results, net::use_awaitable);
+#if defined(BOOST_ASIO_HAS_CO_AWAIT)
 
-        // Set up HTTP GET request
-        http::request<http::string_body> req{http::verb::get, "/", 11};
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+namespace ssl = boost::asio::ssl;
+
+//------------------------------------------------------------------------------
+
+// Performs an HTTP GET and prints the response
+net::awaitable<void> do_session(std::string host, std::string port, std::string target, int version,
+                                ssl::context& ctx) {
+    auto executor = co_await net::this_coro::executor;
+    auto resolver = net::ip::tcp::resolver{executor};
+    auto stream = ssl::stream<beast::tcp_stream>{executor, ctx};
+
+    // Set SNI Hostname (many hosts need this to handshake successfully)
+    if (!SSL_set_tlsext_host_name(stream.native_handle(), host.c_str())) {
+        throw boost::system::system_error(static_cast<int>(::ERR_get_error()), net::error::get_ssl_category());
+    }
+
+    // Look up the domain name
+    auto const results = co_await resolver.async_resolve(host, port);
+
+    // Set the timeout.
+    beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
+
+    // Make the connection on the IP address we get from a lookup
+    co_await beast::get_lowest_layer(stream).async_connect(results);
+
+    // Set the timeout.
+    beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
+
+    // Perform the SSL handshake
+    co_await stream.async_handshake(ssl::stream_base::client);
+
+    // Set the timeout.
+    beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
+
+    // Declare a container to hold the response
+    http::response<http::dynamic_body> res;
+
+    for (const auto i : std::views::iota(0, 1000)) { // Set up an HTTP GET request message
+        http::request<http::string_body> req{http::verb::get, target, version};
+        std::cout << target << std::endl;
+        req.keep_alive(true);
         req.set(http::field::host, host);
         req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-        // Send the request
-        co_await http::async_write(socket, req, net::use_awaitable);
+        // Send the HTTP request to the remote host
+        co_await http::async_write(stream, req);
+        beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
 
-        // Buffer for reading
+        // This buffer is used for reading and must be persisted
         beast::flat_buffer buffer;
 
-        // Container for the response
-        http::response<http::dynamic_body> res;
+        // Receive the HTTP response
+        co_await http::async_read(stream, buffer, res);
+        beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
 
-        // Receive the response
-        co_await http::async_read(socket, buffer, res, net::use_awaitable);
+        auto&& result = res.result();
+        if (result == http::status::ok) {
+            std::cout << res << std::endl;
+            break;
+        }
 
-        // Print the response
-        std::cout << res << std::endl;
+        // Write the message to standard out
+        // std::cout << res << std::endl;
 
-        // Gracefully close the connection
-        beast::error_code ec;
-        socket.shutdown(tcp::socket::shutdown_both, ec);
+        target = res[http::field::location];
+    }
 
-        if (ec && ec != beast::errc::not_connected)
-            throw beast::system_error{ec};
+    // Set the timeout.
+    beast::get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
 
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+    // Gracefully close the stream - do not threat every error as an exception!
+    auto [ec] = co_await stream.async_shutdown(net::as_tuple);
+
+    if (ec && ec != net::ssl::error::stream_truncated) {
+        throw boost::system::system_error(ec, "shutdown");
     }
 }
 
-int main() {
+//------------------------------------------------------------------------------
+
+int main(int argc, char** argv) {
     try {
+        const auto* const host = "ozon.ru";
+        const auto* const port = "443";
+        const auto* const target = "/";
+        // "/product/tualetnaya-bumaga-kix-3-sloya-12-rulonov-1614138493/?avtc=1&avte=4&avts=1733173513";
+        auto const version = 11;
+
+        // The io_context is required for all I/O
         net::io_context ioc;
 
-        // Run the coroutine
-        net::co_spawn(ioc, async_http_get("example.com", "80"), net::detached);
+        // The SSL context is required, and holds certificates
+        ssl::context ctx{ssl::context::tlsv12_client};
 
-        // Run the I/O context
+        // This holds the root certificate used for verification
+        load_root_certificates(ctx);
+
+        // Verify the remote server's certificate
+        ctx.set_verify_mode(ssl::verify_peer);
+
+        // Launch the asynchronous operation
+        net::co_spawn(ioc, do_session(host, port, target, version, ctx),
+                      // If the awaitable exists with an exception, it gets delivered here
+                      // as `e`. This can happen for regular errors, such as connection
+                      // drops.
+                      [](std::exception_ptr e) {
+                          if (e)
+                              std::rethrow_exception(e);
+                      });
+
+        // Run the I/O service. The call will return when
+        // the get operation is complete.
         ioc.run();
-    } catch (const std::exception& e) {
+    } catch (std::exception const & e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
     }
-
     return EXIT_SUCCESS;
 }
+
+#else
+
+int main(int, char*[]) {
+    std::printf("awaitables require C++20\n");
+    return EXIT_FAILURE;
+}
+
+#endif
--- dependencies.cmake
+++ dependencies.cmake
@@ -1,21 +1,21 @@
-include(cmake/get-cpm.cmake)
+set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cpm_packages" CACHE STRING "Directory to download CPM packages")
 
-# Force to use conan packages, in other case try to download them
-set(CPM_USE_LOCAL_PACKAGES ON)
-set(CPM_USE_NAMED_CACHE_DIRECTORIES ON)
+include(cmake/get-cpm.cmake)
 
 # required project dependencies here
 
-cpmaddpackage("gh:fmtlib/fmt#10.2.1") 
+cpmaddpackage("gh:fmtlib/fmt#11.0.2") 
 
 cpmaddpackage(
     GITHUB_REPOSITORY gabime/spdlog 
-    VERSION 1.14.1 
-    OPTIONS "SPDLOG_FMT_EXTERNAL 1" )
+    VERSION 1.15.0 
+    OPTIONS "SPDLOG_FMT_EXTERNAL 1" 
+    SYSTEM)
 
 cpmaddpackage(
     NAME Boost
     VERSION 1.86.0
     URL https://github.com/boostorg/boost/releases/download/boost-1.86.0/boost-1.86.0-cmake.zip
     URL_MD5 86c12489083a48878f8976f9d3e29cb0
-    OPTIONS "BOOST_ENABLE_CMAKE ON")
+    OPTIONS "BOOST_ENABLE_CMAKE ON" "BOOST_INCLUDE_LIBRARIES asio\\\;beast\\\;redis\\\;url\\\;cobalt\\\;program_options\\\;uuid"
+    SYSTEM)
